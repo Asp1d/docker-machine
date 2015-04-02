07dce1eb426e3997ba20d23308028ad31bd1433b
Yanis Tsaunya <yanis88@gmail.com>
2015-04-02 18:32:42 +0300

    namefix 2

--- docker-machine-rc.rb
+++ docker-machine-rc.rb
@@ -1,4 +1,4 @@
-class DockerMachine < Formula
+class DockerMachineRc < Formula
   homepage "https://github.com/docker/machine"
   version "v0.2.0-rc3"
   url "https://github.com/docker/machine/releases/download/#{version}/docker-machine_darwin-amd64"
