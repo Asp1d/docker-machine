f868e4d38e624433a507ac13d87163e8ebae9063
Yanis Tsaunya <yanis88@gmail.com>
2015-04-17 11:22:58 +0300

    sha update

--- docker-machine-rc.rb
+++ docker-machine-rc.rb
@@ -2,7 +2,7 @@ class DockerMachineRc < Formula
   homepage "https://github.com/docker/machine"
   version "v0.2.0"
   url "https://github.com/docker/machine/releases/download/#{version}/docker-machine_darwin-amd64"
-  sha1 "38c508b636e599222574a5565c151e2a4960abb0"
+  sha1 "a1a176a62c2aa3d49eb45710737048c221ba01f5"
   def install
     system "mv docker-machine_darwin-amd64 docker-machine"
     system "chmod +x docker-machine"
