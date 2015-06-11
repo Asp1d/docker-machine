7ce364edda47e7aaa98155446edb472f631b6f97
Yanis Tsaunya <yanis88@gmail.com>
2015-06-11 12:49:01 +0300

    bump version

--- docker-machine-rc.rb
+++ docker-machine-rc.rb
@@ -1,6 +1,6 @@
 class DockerMachineRc < Formula
   homepage "https://github.com/docker/machine"
-  version "v0.2.0"
+  version "v0.3.0-rc2"
   url "https://github.com/docker/machine/releases/download/#{version}/docker-machine_darwin-amd64"
   sha1 "a1a176a62c2aa3d49eb45710737048c221ba01f5"
   def install
